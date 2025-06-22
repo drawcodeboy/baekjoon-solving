d98faae12f638bd52d3040cd931b1007342fc52f
drawcodeboy <20rnjsekdns@naver.com>
2025-06-22 11:12:37 +0900

    [Add] Dijkstra problems (Achieve Gold!)\

--- 01000/1504.cpp
+++ 01000/1504.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#define MAX 1001
-#define INF 1e9
+#define MAX 801
+#define INF 1e7
 using namespace std;
 
-int N, M;
 vector<pair<int, int>> adj[MAX];
 int dist[MAX];
+
+int V, E;
 
 struct cmp{
     bool operator()(pair<int, int> &a, pair<int, int> &b){
@@ -15,44 +16,48 @@ struct cmp{
     }
 };
 
-void dijkstra(int node, int end){
-    for(int i = 1; i <= N; i++){
-        dist[i] = INF;
-    }
+int dijkstra(int node, int end){
+    for(int i = 1; i <= V; i++) dist[i] = INF;
 
     priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
     dist[node] = 0;
     pq.push(make_pair(node, 0));
-    
+
     while(!pq.empty()){
         int now = pq.top().first;
         int cost = pq.top().second;
-        pq.pop();
 
-        if (now == end) return;
+        pq.pop();
 
         for(int i = 0; i < adj[now].size(); i++){
             int next = adj[now][i].first;
             int nextCost = adj[now][i].second;
+
             if(cost + nextCost < dist[next]){
                 dist[next] = cost + nextCost;
                 pq.push(make_pair(next, dist[next]));
             }
         }
     }
+    return dist[end];
 }
 
 int main(){
-    cin >> N >> M;
-    for(int i = 0; i < M; i++){
-        int a, b, d;
-        cin >> a >> b >> d;
-        adj[a].push_back(make_pair(b, d));
-    }
+    cin >> V >> E;
 
+    for(int i = 0; i < E; i++){
+        int u, v, w; cin >> u >> v >> w;
+        adj[u].push_back(make_pair(v, w));
+        adj[v].push_back(make_pair(u, w));
+    }
     int a, b; cin >> a >> b;
-    dijkstra(a, b);
-    cout << dist[b];
+
+    int dist1 = dijkstra(1, a) + dijkstra(a, b) + dijkstra(b, V);
+    int dist2 = dijkstra(1, b) + dijkstra(b, a) + dijkstra(a, V);
+
+    int result = min(dist1, dist2);
+    result = (result < INF) ? result : -1;
+    cout << result;
 
     return 0;
 }
--- 01000/1753.cpp
+++ 01000/1753.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#define MAX 1001
+#define MAX 20001
 #define INF 1e9
 using namespace std;
 
-int N, M;
 vector<pair<int, int>> adj[MAX];
 int dist[MAX];
+
+int V, E;
 
 struct cmp{
     bool operator()(pair<int, int> &a, pair<int, int> &b){
@@ -15,25 +16,23 @@ struct cmp{
     }
 };
 
-void dijkstra(int node, int end){
-    for(int i = 1; i <= N; i++){
-        dist[i] = INF;
-    }
+void dijkstra(int node){
+    for(int i = 1; i <= V; i++) dist[i] = INF;
 
     priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
     dist[node] = 0;
     pq.push(make_pair(node, 0));
-    
+
     while(!pq.empty()){
         int now = pq.top().first;
         int cost = pq.top().second;
-        pq.pop();
 
-        if (now == end) return;
+        pq.pop();
 
         for(int i = 0; i < adj[now].size(); i++){
             int next = adj[now][i].first;
             int nextCost = adj[now][i].second;
+
             if(cost + nextCost < dist[next]){
                 dist[next] = cost + nextCost;
                 pq.push(make_pair(next, dist[next]));
@@ -43,16 +42,19 @@ void dijkstra(int node, int end){
 }
 
 int main(){
-    cin >> N >> M;
-    for(int i = 0; i < M; i++){
-        int a, b, d;
-        cin >> a >> b >> d;
-        adj[a].push_back(make_pair(b, d));
+    cin >> V >> E;
+    int start; cin >> start;
+
+    for(int i = 0; i < E; i++){
+        int u, v, w; cin >> u >> v >> w;
+        adj[u].push_back(make_pair(v, w));
     }
 
-    int a, b; cin >> a >> b;
-    dijkstra(a, b);
-    cout << dist[b];
+    dijkstra(start);
 
-    return 0;
+    for(int i = 1; i <= V; i++){
+        if(dist[i] == INF) cout << "INF";
+        else cout << dist[i];
+        cout << '\n';
+    }
 }
